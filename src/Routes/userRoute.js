712251cddd0ea5583adec0a1c1e6c4070d7d221b
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-16 14:57:34 +0600

    confirmation url changed

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -5,8 +5,8 @@ const { verifyToken } = require('../Middlewares/verifyToken');
 
 router.post("/signup", userController.signUp)
 router.post("/signin", userController.signIn)
-router.get("/signup/confirmation/:token", userController.confirmAccount)
 router.get("/initial", verifyToken, userController.initialSignIn)
+router.get("/confirmation/:token", userController.confirmAccount)
 router.post("/update-profile", verifyToken, userController.updateProfile)
 router.get("/userPersistency", verifyToken, userController.userPersistency)
 
